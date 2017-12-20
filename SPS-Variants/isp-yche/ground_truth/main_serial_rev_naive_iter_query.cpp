67eae7e392725f020e2d9e085a93e93950e02d9a
CHE Yulin <yche@cse.ust.hk>
2017-12-20 16:54:22 +0800

    updaste sling, isp, reads

--- SPS-Variants/isp-yche/ground_truth/main_serial_rev_naive_iter_query.cpp
+++ SPS-Variants/isp-yche/ground_truth/main_serial_rev_naive_iter_query.cpp
@@ -14,7 +14,10 @@ int main(int argc, char *argv[]) {
     string path_str = argv[1];
     double c = atof(argv[2]);
     SerialRevNaiveSim my_algo(path_str);
+    int u = atoi(argv[3]);
+    int v = atoi(argv[4]);
 
     auto &my_mat = my_algo.ComputeSim(100, 0.00001, c);
 //    cout << my_mat << endl;
+    cout << my_mat[u * sqrt(my_mat.size()) + v];
 }
---
+++ SPS-Variants/sling/main_query_all_vertices.cpp
@@ -0,0 +1,59 @@
+//
+// Created by yche on 12/20/17.
+//
+
+//
+// Created by yche on 11/19/17.
+//
+
+#include <iostream>
+
+#include "sling.h"
+#include "graph.h"
+
+using namespace std;
+using namespace std::chrono;
+
+int main(int argc, char *argv[]) {
+    // 1st: load graph
+    Graph g;
+    string file_path = argv[1];
+    g.inputGraph(file_path);
+
+    double c = atof(argv[2]);
+
+    auto tmp_start = std::chrono::high_resolution_clock::now();
+    Sling sling_algo(&g, c);
+    auto tmp_end = std::chrono::high_resolution_clock::now();
+    cout << "finish input graph " << duration_cast<microseconds>(tmp_end - tmp_start).count() << " us\n";
+
+    // build the index
+    cout << "indexing..." << endl;
+
+    tmp_start = std::chrono::high_resolution_clock::now();
+    sling_algo.calcD(0.005);
+    tmp_end = std::chrono::high_resolution_clock::now();
+    cout << "finish calcD " << duration_cast<microseconds>(tmp_end - tmp_start).count() << " us\n";
+
+    tmp_start = std::chrono::high_resolution_clock::now();
+    sling_algo.backward(0.000725);
+    tmp_end = std::chrono::high_resolution_clock::now();
+    cout << "finish backward " << duration_cast<microseconds>(tmp_end - tmp_start).count() << " us\n";
+
+//    int u = atoi(argv[3]);
+//    int v = atoi(argv[4]);
+
+    tmp_start = std::chrono::high_resolution_clock::now();
+#pragma omp parallel for schedule(dynamic, 1)
+    for (auto u = 0; u < sling_algo.g->n; u++) {
+        for (auto v = u; v < sling_algo.g->n; v++) {
+            sling_algo.simrank(u, v);
+        }
+    }
+
+    tmp_end = std::chrono::high_resolution_clock::now();
+
+//    cout << result << std::endl;
+    cout << "query time:"
+         << duration_cast<microseconds>(tmp_end - tmp_start).count() << " us\n";
+}
