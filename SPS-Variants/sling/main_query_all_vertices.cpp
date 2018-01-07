333accd9bc5c11ed2a6241f7b93d8bdec23aead0
CHE Yulin <yche@cse.ust.hk>
2018-01-07 19:07:36 +0800

    update sling, add ground truth checking, require super small delta to pass test

--- SPS-Variants/sling/main_query_all_vertices.cpp
+++ SPS-Variants/sling/main_query_all_vertices.cpp
@@ -8,6 +8,8 @@
 
 #include <iostream>
 
+#include "ground_truth/simrank.h"
+
 #include "sling.h"
 #include "graph.h"
 
@@ -25,7 +27,14 @@ int main(int argc, char *argv[]) {
     auto tmp_start = std::chrono::high_resolution_clock::now();
     Sling sling_algo(&g, c);
     auto tmp_end = std::chrono::high_resolution_clock::now();
+
     cout << "finish input graph " << duration_cast<microseconds>(tmp_end - tmp_start).count() << " us\n";
+
+#ifdef GROUND_TRUTH
+    GraphYche g_gt(file_path);
+    TruthSim ts(string(argv[3]), g_gt, c, 0.01);
+    auto max_err = 0.0;
+#endif
 
     // build the index
     cout << "indexing..." << endl;
@@ -49,18 +58,32 @@ int main(int argc, char *argv[]) {
 //    int v = atoi(argv[4]);
 
     tmp_start = std::chrono::high_resolution_clock::now();
+#ifdef GROUND_TRUTH
+#pragma omp parallel for reduction(max:max_err) schedule(dynamic, 1)
+#else
 #pragma omp parallel for schedule(dynamic, 1)
+#endif
 //    for (auto u = 0; u < sling_algo.g->n; u++) {
     for (auto u = 0; u < 1000; u++) {
 //        for (auto v = u; v < sling_algo.g->n; v++) {
         for (auto v = u; v < 1000; v++) {
+#ifdef GROUND_TRUTH
+            auto res = sling_algo.simrank(u, v);
+            max_err = max(max_err, abs(ts.sim(u, v) - res));
+            if (abs(ts.sim(u, v) - res) > 0.01) {
+#pragma omp critical
+                cout << u << ", " << v << "," << ts.sim(u, v) << "," << res << endl;
+            }
+#else
             sling_algo.simrank(u, v);
+#endif
         }
     }
-
     tmp_end = std::chrono::high_resolution_clock::now();
 
-//    cout << result << std::endl;
+#ifdef GROUND_TRUTH
+    cout << "max err:" << max_err << endl;
+#endif
     cout << "query time:"
          << float(duration_cast<microseconds>(tmp_end - tmp_start).count()) / (pow(10, 6)) << " s\n";
 }
