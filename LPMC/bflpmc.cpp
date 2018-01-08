d180abe9e5a1f2514ad2fd650abb2cfc3b8c0e3c
WANG Yue <ywangby@connect.ust.hk>
2018-01-08 16:11:28 +0800

    fix bug for bflpmc

--- LPMC/bflpmc.cpp
+++ LPMC/bflpmc.cpp
@@ -17,22 +17,22 @@ double BFLPMC::query_one2one(NodePair np){
     }
     
     double blp_p_i = blp->backward_push(np, blp->heap).first;// the estimate value of backward local push
-    double flp_p_i = flp->lp->query_P(np.first, np.second);
+    cout << "BLP result: " << blp_p_i << endl;
 
-    double mc_r_i = 0;
-
-    int total_num_samples = 0;
     // perform sampling: MC3
-    auto begin = blp->heap.heap.begin();
-    auto end = blp->heap.heap.end();
-    double r_sum = blp->heap.sum;
+
     int N = blp->number_of_walkers(blp->heap.sum);
+
+    cout << "number of samples: " << N << endl;
     
     // set up the random number generator
     std::random_device rd;  //Will be used to obtain a seed for the random number engine
     std::mt19937 generator(rd()); //Standard mersenne_twister_engine seeded with rd()
     std::uniform_real_distribution<double> distribution(0.0,1.0);
     // set up the discret distribution
+    auto begin = blp->heap.heap.begin();
+    auto end = blp->heap.heap.end();
+    double r_sum = blp->heap.sum;
     vector<double> weights;
     vector<NodePair> node_pairs;
     for(auto it = begin; it !=end; ++ it){
@@ -50,8 +50,9 @@ double BFLPMC::query_one2one(NodePair np){
         int a,b;
         tie(a,b) = sampled_np;
         // samples from this node pair
-        current_residual += flp->lp->query_R(sampled_np.first, sampled_np.second);
-        while(distribution(generator) < c && (a !=b )){
+        current_residual += flp->lp->query_R(a, b);
+        double current_estimate = flp->lp->query_P(a, b);
+        while( (distribution(generator) < c) && (a !=b )){
             a = sample_in_neighbor(a, *g);
             b = sample_in_neighbor(b, *g);
             if(a == -1 || b == - 1){
@@ -59,34 +60,9 @@ double BFLPMC::query_one2one(NodePair np){
             }
             current_residual += flp->lp->query_R(a, b);
         }
-        estimate_r_i += current_residual / N;
+        estimate_r_i += (current_residual + current_estimate) / N;
     }
-    return blp_p_i + estimate_r_i * blp->heap.sum;
+    cout << "avg. residual sum: " << estimate_r_i << endl;
+    return blp_p_i + estimate_r_i * r_sum;
 
-    // cout << format("total number of samples: %s") % N << endl;
-    // if(N>0){
-    //     for(auto it = begin; it !=end; ++it){
-    //         double residual = (*it).residual;
-    //         int n = round(residual * N / r_sum);
-    //         total_num_samples += n;
-    //         for(int i = 0 ; i< n;++i){
-    //             double sum_in_this_round = 0;
-    //             int a,b; // the starting node pair
-    //             tie(a,b) = (*it).np;
-    //             sum_in_this_round += flp->lp->query_R(np.first, np.second);
-    //             while(distribution(gen) < c && (a != b)){
-    //                 a = sample_in_neighbor(a, *g);
-    //                 b = sample_in_neighbor(b, *g);
-    //                 if(a == -1 || b == -1){
-    //                     break;
-    //                 }
-    //                 sum_in_this_round += flp->lp->query_R(a,b);
-    //             }
-    //             estimate_r_i += sum_in_this_round;
-    //         }
-    //     }
-    //     estimate_r_i = estimate_r_i / total_num_samples;
-    // }
-    
-    // return blp_p_i + flp_p_i + estimate_r_i;
 }
