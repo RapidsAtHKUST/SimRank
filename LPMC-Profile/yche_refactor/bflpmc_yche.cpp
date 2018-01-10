2ea65b719c765e7ad06b97b0f71375d17de7bde2
CHE Yulin <yche@cse.ust.hk>
2018-01-10 17:10:00 +0800

    update bflpmc impl, add a condition

--- LPMC-Profile/yche_refactor/bflpmc_yche.cpp
+++ LPMC-Profile/yche_refactor/bflpmc_yche.cpp
@@ -21,49 +21,58 @@ double BFLPMC::query_one2one(NodePair np) {
         return blp_p_i;
     }
 
-    double h_p_r = 0;
+    double h_p_r = 0;  // for the results of termination residual model
     // perform sampling: MC3
 
     int N = blp->number_of_walkers(blp->heap.sum);
+    // consider the case directly using \sqrt{c} meeting interpretation BLPMC method now
+    int N2 = ceil(pow(c * blp->heap.sum / epsilon, 2.0) * log(2 / delta) / 2);
 
 #ifdef DEBUG
     cout << "number of samples: " << N << endl;
 #endif
-    // set up the random number generator
-    std::random_device rd;  //Will be used to obtain a seed for the random number engine
-    std::mt19937 generator(rd()); //Standard mersenne_twister_engine seeded with rd()
+    if (N2 < N) { // using the c-walk meeting model
+        cout << "number of samples N2: " << N2 << endl;
 
-    // set up the discret distribution
-    auto begin = blp->heap.heap.begin();
-    auto end = blp->heap.heap.end();
-    double r_sum = blp->heap.sum;
-    vector<double> weights;
-    vector<NodePair> node_pairs;
-    for (auto it = begin; it != end; ++it) {
-        weights.push_back((*it).residual / blp->heap.sum);
-        NodePair current_np = (*it).np;
-        node_pairs.push_back(current_np);
-        // update hpr component
-        h_p_r += (*it).residual * (flp->lp->query_P(current_np.first, current_np.second) +
-                                   flp->lp->query_R(current_np.first, current_np.second));
-    }
+        double estimate_s_i = blp->MC_random_walk(N2);
+        return blp_p_i + estimate_s_i;
+    } else {
+
+        // set up the random number generator
+        std::random_device rd;  //Will be used to obtain a seed for the random number engine
+        std::mt19937 generator(rd()); //Standard mersenne_twister_engine seeded with rd()
+
+        // set up the discret distribution
+        auto begin = blp->heap.heap.begin();
+        auto end = blp->heap.heap.end();
+        double r_sum = blp->heap.sum;
+        vector<double> weights;
+        vector<NodePair> node_pairs;
+        for (auto it = begin; it != end; ++it) {
+            weights.push_back((*it).residual / blp->heap.sum);
+            NodePair current_np = (*it).np;
+            node_pairs.push_back(current_np);
+            // update hpr component
+            h_p_r += (*it).residual * (flp->lp->query_P(current_np.first, current_np.second) +
+                                       flp->lp->query_R(current_np.first, current_np.second));
+        }
 //    std::discrete_distribution<int> residuals_dist(weights.begin(), weights.end());
-    constexpr int YCHE_MAX_INT = 1 << 30;
-    vector<int> cdf(weights.size(), 0);
-    auto prev = 0.0;
-    auto accumulation = prev;
-    for (auto i = 0; i < weights.size(); i++) {
-        accumulation = prev + weights[i];
-        cdf[i] = static_cast<int>(accumulation * YCHE_MAX_INT);
-        prev = accumulation;
-    }
-    cdf.back() = YCHE_MAX_INT;
+        constexpr int YCHE_MAX_INT = 1 << 30;
+        vector<int> cdf(weights.size(), 0);
+        auto prev = 0.0;
+        auto accumulation = prev;
+        for (auto i = 0; i < weights.size(); i++) {
+            accumulation = prev + weights[i];
+            cdf[i] = static_cast<int>(accumulation * YCHE_MAX_INT);
+            prev = accumulation;
+        }
+        cdf.back() = YCHE_MAX_INT;
 
-    // begin sampling
-    double estimate_r_i = 0;
-    for (int i = 0; i < N; i++) {
-        double terminate_r = 0;
-        int step = 0;
+        // begin sampling
+        double estimate_r_i = 0;
+        for (int i = 0; i < N; i++) {
+            double terminate_r = 0;
+            int step = 0;
 //        int index = residuals_dist(generator); // index for node pairs
 //        int index = BinarySearchForGallopingSearch(reinterpret_cast<const double *>(&cdf.front()), 0, cdf.size(),
 //                                                   rand_gen.double_rand());
@@ -71,30 +80,30 @@ double BFLPMC::query_one2one(NodePair np) {
 //                                    static_cast<int>(rand_gen.double_rand() * YCHE_MAX_INT));
 //        int index = GallopingSearchAVX2(&cdf.front(), 0, static_cast<uint32_t>(cdf.size()),
 //                                                       static_cast<int>(rand_gen.double_rand() * YCHE_MAX_INT));
-        int index = BinarySearchForGallopingSearchAVX2(&cdf.front(), 0, static_cast<uint32_t>(cdf.size()),
-                                                       static_cast<int>(rand_gen.double_rand() * YCHE_MAX_INT));
+            int index = BinarySearchForGallopingSearchAVX2(&cdf.front(), 0, static_cast<uint32_t>(cdf.size()),
+                                                           static_cast<int>(rand_gen.double_rand() * YCHE_MAX_INT));
 
-        NodePair sampled_np = node_pairs[index];
-        int a, b;
-        tie(a, b) = sampled_np;
-        // samples from this node pair
+            NodePair sampled_np = node_pairs[index];
+            int a, b;
+            tie(a, b) = sampled_np;
+            // samples from this node pair
 
-        double current_estimate = flp->lp->query_P(a, b);
-        while (((rand_gen.double_rand() < c) || step == 0) && (a != b)) { // random walk length (1 + 1 / (1-c))
-            a = sample_in_neighbor(a, *g, rand_gen);
-            b = sample_in_neighbor(b, *g, rand_gen);
-            step++;
-            if (a == -1 || b == -1) {
-                break;
+            double current_estimate = flp->lp->query_P(a, b);
+            while (((rand_gen.double_rand() < c) || step == 0) && (a != b)) { // random walk length (1 + 1 / (1-c))
+                a = sample_in_neighbor(a, *g, rand_gen);
+                b = sample_in_neighbor(b, *g, rand_gen);
+                step++;
+                if (a == -1 || b == -1) {
+                    break;
+                }
             }
+            if (a != -1 && b != -1) {
+                terminate_r = flp->lp->query_R(a, b);
+            }
+            estimate_r_i += terminate_r / N;
         }
-        if (a != -1 && b != -1) {
-            terminate_r = flp->lp->query_R(a, b);
-        }
-        estimate_r_i += terminate_r / N;
+        return blp_p_i + h_p_r + c * r_sum * estimate_r_i / (1 - c);
     }
-
-    return blp_p_i + h_p_r + c * r_sum * estimate_r_i / (1 - c);
 }
 
 BFLPMC::BFLPMC(const BFLPMC &other_obj) {
