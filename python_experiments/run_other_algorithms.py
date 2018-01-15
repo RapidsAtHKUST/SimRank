9030bac898b60011251e1c5384fd8b66752fe366
CHE Yulin <yche@cse.ust.hk>
2018-01-15 21:49:28 +0800

    update scripts and sling path

--- python_experiments/run_other_algorithms.py
+++ python_experiments/run_other_algorithms.py
@@ -32,26 +32,24 @@ def signal_handler(signal, frame):
 
 
 def run_exp():
-    our_exec_path = '/homes/ywangby/workspace/yche/git-repos/SimRank/LPMC-Profile/build'
-    our_exec_name_lst = ['bprw-ap', 'flpmc-ap', 'bflpmc-ap']
+    other_exec_path_lst = ['/homes/ywangby/workspace/yche/git-repos/SimRank/SPS-Variants/sling/build/sling_all']
     data_set_lst = ['ca-GrQc', 'ca-HepTh', 'p2p-Gnutella06', 'wiki-Vote']
-    folder_name = 'our_methods_overview'
+    folder_name = 'other_methods_overview'
     tag = 'exp_results'
 
     def one_round():
         for data_set_name in data_set_lst:
-            for our_algorithm in our_exec_name_lst:
+            for other_algorithm_path in other_exec_path_lst:
                 statistics_dir = os.sep.join(map(str, ['.', tag, folder_name, data_set_name]))
                 os.system('mkdir -p ' + statistics_dir)
-                statistics_file_path = statistics_dir + os.sep + our_algorithm + '.txt'
+                statistics_file_path = statistics_dir + os.sep + other_algorithm_path.split('/')[-1] + '.txt'
 
                 # 1st: write header
                 os.system(' '.join(
                     ['echo', my_splitter + time.ctime() + my_splitter, '>>', statistics_file_path]))
-                algorithm_path = our_exec_path + os.sep + our_algorithm
-                params_lst = map(str, [algorithm_path, data_set_name, '>>', statistics_file_path])
+                params_lst = map(str, [other_algorithm_path, data_set_name, '>>', statistics_file_path])
                 cmd = ' '.join(params_lst)
-                time_out = 1000 if data_set_name != 'soc-LiveJournal1' else 3600
+                time_out = 1200 if data_set_name != 'soc-LiveJournal1' else 3600
 
                 tle_flag, info, correct_info = time_out_util.run_with_timeout(cmd, timeout_sec=time_out)
                 write_split(statistics_file_path)
@@ -66,8 +64,9 @@ def run_exp():
     one_round()
     data_set_lst = ['ca-GrQc', 'ca-HepTh', 'p2p-Gnutella06', 'wiki-Vote',
                     'email-Enron', 'email-EuAll', 'web-NotreDame', 'web-Stanford', 'web-BerkStan', 'web-Google',
-                    'cit-Patents', 'soc-LiveJournal1']
-    our_exec_name_lst = ['bprw-bench', 'flpmc-bench', 'bflpmc-bench']
+                    'cit-Patents', 'soc-LiveJournal1'
+                    ]
+    other_exec_path_lst = ['/homes/ywangby/workspace/yche/git-repos/SimRank/SPS-Variants/sling/build/sling-bench']
     one_round()
 
 
