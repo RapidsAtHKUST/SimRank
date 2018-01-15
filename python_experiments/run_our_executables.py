16f09823f5579f1ad24c4399d24083ddcf501ae3
CHE Yulin <yche@cse.ust.hk>
2018-01-15 17:18:55 +0800

    update script fix typo

--- python_experiments/run_our_executables.py
+++ python_experiments/run_our_executables.py
@@ -1,15 +1,28 @@
 import commands
-import os
 import sys
 import time
 
+import os
+
+import time_out_util
+
+my_splitter = '-'.join(['*' for _ in xrange(20)])
+
 
 def kill_all():
-    err_code, output = commands.getstatusoutput("ps -ef | grep cuda | awk '{print $2}'")
-
-    for pid in output.strip().split('\n'):
-        os.system('kill -9 ' + pid)
+    # kill *-bench
+    exec_name_lst = []
+    for exec_name in exec_name_lst:
+        err_code, output = commands.getstatusoutput("ps -ef | grep " + exec_name + " | awk '{print $2}'")
+        for pid in output.strip().split('\n'):
+            os.system('kill -9 ' + pid)
     time.sleep(5)
+
+
+def write_split(statistics_file_path):
+    with open(statistics_file_path, 'a+') as ifs:
+        ifs.write(my_splitter + my_splitter + '\n')
+        ifs.write(my_splitter + my_splitter + '\n')
 
 
 def signal_handler(signal, frame):
@@ -18,56 +31,43 @@ def signal_handler(signal, frame):
     sys.exit(0)
 
 
-def ground_truth_exp():
-    our_exec_path = '/homes/ywangby/workspace/yche/git-repos/SimRank/LPMC-Profile/build/'
-    our_ap_exec_name_lst = ['bprw-ap', 'flpmc-ap', 'bflpmc-ap']
-    data_set_lst = ["ca-HepPh", "ca-GrQc", "p2p-Gnutella06", "wiki-Vote"]
+def run_exp():
+    our_exec_path = '/homes/ywangby/workspace/yche/git-repos/SimRank/LPMC-Profile/build'
+    our_exec_name_lst = ['bprw-ap', 'flpmc-ap', 'bflpmc-ap']
+    data_set_lst = ['ca-GrQc', 'ca-HepTh', 'p2p-Gnutella06', 'wiki-Vote']
     folder_name = 'our_methods_overview'
+    tag = 'exp_results'
 
-    for dataset in data_set_lst:
-        pass
+    def one_round():
+        for data_set_name in data_set_lst:
+            for our_algorithm in our_exec_name_lst:
+                statistics_dir = os.sep.join(map(str, ['.', tag, folder_name, data_set_name]))
+                os.system('mkdir -p ' + statistics_dir)
+                statistics_file_path = statistics_dir + os.sep + our_algorithm + '.txt'
+
+                # 1st: write header
+                os.system(' '.join(
+                    ['echo', my_splitter + time.ctime() + my_splitter, '>>', statistics_file_path]))
+                algorithm_path = our_exec_path + os.sep + our_algorithm
+                params_lst = map(str, [algorithm_path, data_set_name, '>>', statistics_file_path])
+                cmd = ' '.join(params_lst)
+                tle_flag, info, correct_info = time_out_util.run_with_timeout(cmd, timeout_sec=1000)
+                write_split(statistics_file_path)
+
+                with open(statistics_file_path, 'a+') as ifs:
+                    ifs.write(correct_info)
+                    ifs.write(my_splitter + time.ctime() + my_splitter)
+                    ifs.write('is_time_out:' + str(tle_flag))
+                    ifs.write('\n\n\n\n')
+                print 'finish:', cmd
+
+    one_round()
+    data_set_lst = ['ca-GrQc', 'ca-HepTh', 'p2p-Gnutella06', 'wiki-Vote',
+                    'email-Enron', 'email-EuAll', 'web-NotreDame', 'web-Stanford', 'web-BerkStan', 'web-Google',
+                    'cit-Patents', 'soc-LiveJournal1']
+    our_exec_name_lst = ['bprw-bench', 'flpmc-bench', 'bflpmc-bench']
+    one_round()
 
 
 if __name__ == '__main__':
-    pass
-# data_set_lst = [
-#     "ca-HepPh", "ca-GrQc", "p2p-Gnutella06", "wiki-Vote",
-#     "web-NotreDame", "web-Stanford", "web-BerkStan", "web-Google",
-#     "soc-LiveJournal1",
-# ]
-#
-# our_exec_path = '/homes/ywangby/workspace/yche/git-repos/SimRank/LPMC-Profile/build/'
-#
-# our_benchmark_exec_name_lst = ['bprw-bench', 'flpmc-bench', 'bflpmc-bench']
-#
-# for data_set_name in data_set_lst:
-#     statistics_dir = os.sep.join(map(str, ['.', folder_name, data_set_name]))
-#     os.system('mkdir -p ' + statistics_dir)
-#
-#     statistics_file_path = statistics_dir + os.sep + '.txt'
-#
-#     # header
-#     my_splitter = '-'.join(['*' for _ in xrange(20)])
-#     os.system(' '.join(
-#         ['echo', my_splitter + time.ctime() + my_splitter, '>>', statistics_file_path]))
-#
-#
-#     def write_split():
-#         with open(statistics_file_path, 'a+') as ifs:
-#             ifs.write(my_splitter + my_splitter + '\n')
-#             ifs.write(my_splitter + my_splitter + '\n')
-#
-#
-#     params_lst = map(str, [any_scan_path, '>>', statistics_file_path])
-#     cmd = ' '.join(params_lst)
-#     print cmd
-#     tle_flag, info, correct_info = time_out_util.run_with_timeout(cmd, timeout_sec=1000)
-#     write_split()
-#
-#     with open(statistics_file_path, 'a+') as ifs:
-#         ifs.write(correct_info)
-#         ifs.write(my_splitter + time.ctime() + my_splitter)
-#         ifs.write('is_time_out:' + str(tle_flag))
-#         ifs.write('\n\n\n\n')
-#     print 'finish:', '-'.join(map(str, [data_set_path, eps, min_pts]))
-# print
+    run_exp()
