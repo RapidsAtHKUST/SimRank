79b8a65cf7f0c307c947cdcd551180dc938354b0
CHE Yulin <yche@cse.ust.hk>
2018-01-26 20:18:05 +0800

    update drawing max err figure

--- python_experiments/paper_figures/draw_varying_eps_max_err.py
+++ python_experiments/paper_figures/draw_varying_eps_max_err.py
@@ -17,20 +17,20 @@ eps_max_err_dict = get_dict('../data_analysis/data-json/varying_parameters/varyi
 def draw_max_err():
     eps_lst = [0.001 * (i + 1) for i in xrange(1, 15, 1)]
     xtick_lst = [0.005, 0.01, 0.015]
-    exp_figure, ax_tuple = plt.subplots(1, 1, sharex=True, figsize=(8, 4))
+    exp_figure, ax_tuple = plt.subplots(1, 1, sharex=True, figsize=(7, 4))
 
     # 1st: draw querying time
     algorithm_tag_lst = [bflpmc_tag, flpmc_tag, bprw_tag, sling_tag,
                          reads_d_tag, reads_rq_tag, isp_tag, tsf_tag]
     legend_lst = ['FBLPMC', 'FLPMC', 'BLPMC', 'SLING',
-                  'READS-D', 'READS-RQ', 'ISP', 'TSF']
+                  'READS-D', 'READS-RQ', 'ISP', 'TSF', 'Bound']
 
     ax = ax_tuple
     lst_lst = []
     for idx, algorithm in enumerate(algorithm_tag_lst):
         max_err_lst = map(lambda eps: eps_max_err_dict[algorithm][format_str(eps)], eps_lst)
 
-        max_err_lst = map(lambda err: 0.025 if err > 0.025 else err, max_err_lst)
+        max_err_lst = map(lambda err: 0.015 if err > 0.015 else err, max_err_lst)
         lst_lst.append(max_err_lst)
         color_lst = ['blue', 'orange', 'green', 'red',
                      '#fe01b1', '#ceb301', 'm', 'brown', 'k', 'gray']
@@ -49,25 +49,24 @@ def draw_max_err():
         ax.plot(eps_lst, max_err_lst, shape_lst[idx], color=color_lst[idx], markersize=get_marker_size(idx),
                 markerfacecolor='none')
         # ax.set_yscale('log')
-
+    ax.plot(eps_lst, eps_lst, '--', color='black', markerfacecolor='none')
     # setup ticks for x and y axis
-    # ax.set_ylim(0.8 / us_to_ms_factor, 10 ** 10 * 4 / us_to_ms_factor)
-    ax.set_ylim(0, 0.028)
 
-    # ax.set_yticks(map(lambda digit: 10 ** (-digit), [4, 3, 2, 1, 0]))
+    ax.set_ylim(0, 0.016)
+    ax.set_yticks([0, 0.005, 0.010, 0.015])
+    ax.set_yticklabels(['0', '0.005', '0.010', '>0.015'])
     ax.set_xticks(xtick_lst)
     for tick in ax.yaxis.get_major_ticks():
         tick.label.set_fontsize(TICK_SIZE)
     for tick in ax.xaxis.get_major_ticks():
         tick.label.set_fontsize(TICK_SIZE)
     # setup labels and grid, legend
-    ax.set_ylabel('Maximum Error', fontsize=LABEL_SIZE)
+    ax.set_ylabel('Maximum Error', fontsize=LABEL_SIZE+2)
     ax.set_xlabel('$\\epsilon$', fontsize=LABEL_SIZE)
     ax.grid(True, alpha=0.4)
-    # ax.legend(legend_lst, ncol=2, prop={'size': LEGEND_SIZE, "weight": "bold"}, loc=1)
     plt.tight_layout()
     # plt.subplots_adjust(right=0.5)
-    plt.legend(legend_lst, ncol=1, fontsize=LEGEND_SIZE, prop={'size': LEGEND_SIZE, "weight": "bold"},
+    plt.legend(legend_lst, ncol=1, fontsize=LEGEND_SIZE, prop={'size': LEGEND_SIZE - 2, "weight": "bold"},
                bbox_to_anchor=(1.05, 1.1))
     exp_figure.subplots_adjust(wspace=0)
     plt.tight_layout()
