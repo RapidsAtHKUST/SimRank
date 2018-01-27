a58369b16a79ee18efd982146d95a5f6263fa2ee
CHE Yulin <yche@cse.ust.hk>
2018-01-27 18:34:15 +0800

    update max-err varying eps figure

--- python_experiments/paper_figures/draw_varying_eps_max_err.py
+++ python_experiments/paper_figures/draw_varying_eps_max_err.py
@@ -15,7 +15,7 @@ eps_max_err_dict = get_dict('../data_analysis/data-json/varying_parameters/varyi
 def draw_max_err():
     eps_lst = [0.001 * (i + 1) for i in xrange(1, 15, 1)]
     xtick_lst = [0, 0.005, 0.01, 0.015]
-    exp_figure, ax_tuple = plt.subplots(1, 1, sharex=True, figsize=(9, 5))
+    exp_figure, ax_tuple = plt.subplots(1, 1, sharex=True, figsize=(10.5, 5))
 
     # 1st: draw querying time
     algorithm_tag_lst = [bflpmc_tag, flpmc_tag, bprw_tag, sling_tag,
@@ -46,11 +46,11 @@ def draw_max_err():
 
         ax.plot(eps_lst, max_err_lst, shape_lst[idx], color=color_lst[idx], markersize=get_marker_size(idx),
                 markerfacecolor='none')
-    total_eps_lst = [0.001 * i for i in xrange(16)]
+    total_eps_lst = [0.001 * i for i in xrange(17)]
     ax.plot(total_eps_lst, total_eps_lst, '--', color='black', markerfacecolor='none')
     # setup ticks for x and y axis
 
-    ax.set_xlim(0, 0.016)
+    ax.set_xlim(0,0.016)
     ax.set_ylim(0, 0.016)
     ax.set_yticks([0, 0.005, 0.010, 0.015])
     ax.set_yticklabels(['0', '0.005', '0.010', '> 0.015'])
@@ -61,12 +61,12 @@ def draw_max_err():
         tick.label.set_fontsize(TICK_SIZE)
 
     # setup labels and grid, legend
-    ax.set_ylabel('Maximum Error', fontsize=LABEL_SIZE + 2)
+    ax.set_ylabel('Maximum Error', fontsize=LABEL_SIZE + 3)
     ax.set_xlabel('$\\epsilon$', fontsize=LABEL_SIZE)
     ax.grid(True, alpha=0.4)
     plt.tight_layout()
     plt.legend(legend_lst, ncol=1, fontsize=LEGEND_SIZE, prop={'size': LEGEND_SIZE, "weight": "bold"},
-               bbox_to_anchor=(1.05, 1.0))
+               bbox_to_anchor=(1.0, 1.05))
     exp_figure.subplots_adjust(wspace=0)
     plt.tight_layout()
     plt.savefig('figures/' + 'varying_eps_max_err' + '.pdf', bbox_inches='tight', dpi=300)
