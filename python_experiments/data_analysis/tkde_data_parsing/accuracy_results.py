4aa7c65d7cc6066b81b1090d9ef95baf95fa51ac
CHE Yulin <yche@cse.ust.hk>
2019-09-19 10:36:11 +0800

    finish the accuracy results (adding SLING/ProbeSim/READS-D/READS-Rq) latex table

--- python_experiments/data_analysis/tkde_data_parsing/accuracy_results.py
+++ python_experiments/data_analysis/tkde_data_parsing/accuracy_results.py
@@ -1,7 +1,6 @@
 from config import *
 from data_analysis.util.read_file_utils_updated import *
 from data_analysis.util.parsing_helpers import *
-from exec_utilities.exec_utils import get_logger
 from data_analysis.tkde_data_parsing.key_word_tags import *
 import json
 
@@ -13,8 +12,6 @@ if __name__ == '__main__':
     thread_num_lst = my_config_dict[thread_num_lst_tag]
 
     folder_name = 'exp-ground-truth-2019-09-18'
-    our_exec_name_lst = ['probesim_ss_ap', 'readsrq-ap', 'readsd-ap', 'sling_ss_ap']
-
     log_dir = '/home/yche/logs/{}'.format(lccpu12_tag)
     logger = get_logger(log_dir + 'accuracy_results.log', name=__name__)
     exp_res_root_mount_path_tag = os.sep.join(
@@ -23,25 +20,43 @@ if __name__ == '__main__':
     os.system('mkdir -p {}'.format(json_dir_tag))
     os.system('mkdir -p {}'.format(md_dir_tag))
 
-    json_dict = dict()
-    # Algorithm -> Dataset -> Info-Dictionary (Max Error / Avg Error)
-    for our_algorithm in our_exec_name_lst:
-        my_dict = dict()
-        for data_set_name in gt_data_set_lst:
-            t_num = thread_num_lst[0]
-            statistics_dir = os.sep.join(
-                map(str, [exp_res_root_mount_path_tag, data_set_name, t_num]))
-            log_path = os.sep.join([statistics_dir, our_algorithm + '.log'])
-            logger.info(log_path)
-            lines = get_file_log_lines(log_path)
 
-            info = dict()
-            for tag in [max_err_tag, avg_err_tag]:
-                res_lst = parse_line_split(list(filter(lambda x: contains([max_err_tag], x), lines))[0])
-                info[tag] = None if len(res_lst) == 0 else res_lst[-1]
-            # logger.info(my_dict)
-            my_dict[data_set_name] = info
-        json_dict[our_algorithm] = my_dict
-    logger.info(json_dict)
-    with open('{}/accuracy_result.json'.format(json_dir_tag), 'w') as ofs:
-        ofs.write(json.dumps(json_dict, indent=4))
+    def parse_results():
+        json_dict = dict()
+        # Algorithm -> Dataset -> Info-Dictionary (Max Error / Avg Error)
+        for our_algorithm in accuracy_exec_lst:
+            my_dict = dict()
+            for data_set_name in gt_data_set_lst:
+                t_num = thread_num_lst[0]
+                statistics_dir = os.sep.join(
+                    map(str, [exp_res_root_mount_path_tag, data_set_name, t_num]))
+                log_path = os.sep.join([statistics_dir, our_algorithm + '.log'])
+                logger.info(log_path)
+                lines = get_file_log_lines(log_path)
+
+                info = dict()
+                for tag in [max_err_tag, avg_err_tag]:
+                    res_lst = parse_line_split(list(filter(lambda x: contains([tag], x), lines))[0])
+                    info[tag] = None if len(res_lst) == 0 else '{0:.5e}'.format(float(res_lst[-1]))
+                # logger.info(my_dict)
+                my_dict[data_set_name] = info
+            json_dict[our_algorithm] = my_dict
+        logger.info(json_dict)
+        with open('{}/accuracy_result.json'.format(json_dir_tag), 'w') as ofs:
+            ofs.write(json.dumps(json_dict, indent=4))
+
+
+    # parse_results()
+    def generate_latex_table():
+        with open('{}/accuracy_result.json'.format(json_dir_tag)) as ifs:
+            data_dict = json.load(ifs)
+            for our_algorithm in [sling_ss_ap, probesim_aps_tag, readsd_aps_tag, readsrq_aps_tag]:
+                lst = [alias_dict[our_algorithm]]
+                for data_set in gt_data_set_lst:
+                    for tag in [max_err_tag, avg_err_tag]:
+                        lst.append(get_err_by_tag(data_dict, our_algorithm, data_set, tag))
+                logger.info(' & '.join([str(x) for x in lst]) + ' \\\\ \hline')
+
+
+    parse_results()
+    generate_latex_table()
--- python_experiments/data_analysis/tkde_data_parsing/key_word_tags.py
+++ python_experiments/data_analysis/tkde_data_parsing/key_word_tags.py
@@ -6,6 +6,20 @@ from exec_utilities.exec_utils import get_logger
 max_err_tag = 'Max Error'
 avg_err_tag = 'Avg Error'
 
+probesim_aps_tag = 'probesim_ss_ap'
+readsrq_aps_tag = 'readsrq-ap'
+readsd_aps_tag = 'readsd-ap'
+sling_ss_ap = 'readsrq-ap'
+
+accuracy_exec_lst = [probesim_aps_tag, readsrq_aps_tag, readsd_aps_tag, sling_ss_ap]
+
+alias_dict = {
+    probesim_aps_tag: 'ProbeSim',
+    readsrq_aps_tag: 'READS-Rq',
+    readsd_aps_tag: 'READS-D',
+    sling_ss_ap: 'SLING'
+}
+
 
 # Algorithm -> Dataset -> Info-Dictionary (Max Error / Avg Error)
 def get_err_by_tag(my_dict, algorithm, data_set, tag):
